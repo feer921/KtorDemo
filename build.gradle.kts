8523c55404a9fb7cef6648eeede0ff0526f8fc51
lifei <lifei@iboxpay.com>
2021-02-09 16:43:30 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    application
+}
+
+group = "me.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    implementation("io.ktor:ktor-server-netty:1.5.1")
+    implementation("io.ktor:ktor-html-builder:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "ServerKt"
+}
